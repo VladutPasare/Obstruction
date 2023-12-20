9216aadc4b94a721cb43c742d77b217e68e6235b
vladutpasare12 <vladutflaviuspasare@gmail.com>
2023-12-20 09:06:52 +0200

    initial commit

---
+++ gui.py
@@ -0,0 +1,151 @@
+import pygame
+import time
+from pygame.locals import MOUSEBUTTONDOWN
+
+from board import Board, Cell
+from client import Client
+from strategy import RandomStrategy
+
+
+class GUI:
+    __WAIT_TIME = 1.5
+    __MY_TURN = 1
+    __ENEMY_TURN = 0
+    __WINDOW_CAPTION = "Obstruction"
+
+    def __init__(self, board: Board):
+        self.__board = board
+        pygame.init()
+        pygame.display.set_caption(self.__WINDOW_CAPTION)
+        self.__window = pygame.display.set_mode((600, 600))
+        self.__window.fill((255, 255, 255))
+        self.__score = 0
+        self.__FONT = pygame.font.SysFont("sfcamera", 20)
+        self.__TEXT_COLOR = (0, 0, 0)
+
+    def __display_board(self):
+        for row in range(self.__board.rows):
+            for column in range(self.__board.columns):
+                state = self.__board.get_cell_state(row, column)
+                cell_image = pygame.image.load("assets/free_cell.png")
+                if state == Cell.Wall:
+                    cell_image = pygame.image.load("assets/wall_cell.png")
+                if state == Cell.X:
+                    cell_image = pygame.image.load("assets/x_cell.png")
+                if state == Cell.O:
+                    cell_image = pygame.image.load("assets/o_cell.png")
+                self.__window.blit(cell_image, (self.__board.left + column * 50, self.__board.top + row * 50))
+
+    def __click_on_board(self, x: int, y: int) -> bool:
+        return self.__board.top <= x <= self.__board.top + self.__board.rows * 50 and self.__board.left <= y <= self.__board.left + self.__board.columns * 50
+
+    def __get_row(self, x: int) -> int:
+        return (x - self.__board.top) // 50
+
+    def __get_column(self, y: int) -> int:
+        return (y - self.__board.left) // 50
+
+    def __display_score(self):
+        pygame.draw.rect(self.__window, (255, 255, 255), (500, 30, 100, 30))
+        text = self.__FONT.render(f'Score: {self.__score}', True, self.__TEXT_COLOR, (255, 255, 255))
+        self.__window.blit(text, (500, 30))
+
+    def single_player_mode(self):
+        random_strategy = RandomStrategy(self.__board, Cell.O)
+        computer = 0
+        player = 1
+        turn = computer
+        game_over = False
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+
+                self.__display_score()
+                self.__display_board()
+                pygame.display.update()
+
+                if game_over:
+                    game_over = False
+                    time.sleep(self.__WAIT_TIME)
+                    self.__board.reset()
+                    self.__window.fill((255, 255, 255))
+
+                if turn == computer:
+                    time.sleep(0.2)
+                    random_strategy.move()
+                    turn = player
+
+                    if not self.__board.free_cells():
+                        you_lost_message = pygame.image.load("assets/you_lost.png")
+                        self.__window.blit(you_lost_message, (140, 40))
+                        game_over = True
+                        if self.__score > 0:
+                            self.__score -= 1
+                else:
+                    if event.type == MOUSEBUTTONDOWN:
+                        y, x = pygame.mouse.get_pos()
+
+                        if self.__click_on_board(x, y):
+                            row = self.__get_row(x)
+                            column = self.__get_column(y)
+
+                            if self.__board.get_cell_state(row, column) == Cell.Free:
+                                self.__board.set_cell_state(row, column, Cell.X)
+                                turn = computer
+                                if not self.__board.free_cells():
+                                    you_won_message = pygame.image.load("assets/you_won.png")
+                                    self.__window.blit(you_won_message, (140, 40))
+                                    game_over = True
+                                    self.__score += 1
+
+    def multiplayer_mode(self):
+        client = Client(5240)
+        turn = client.turn
+        game_over = 0
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+
+                self.__display_score()
+                self.__display_board()
+                pygame.display.update()
+
+                if game_over:
+                    self.__board.reset()
+                    self.__window.fill((255, 255, 255))
+                    time.sleep(self.__WAIT_TIME)
+                    game_over = 0
+                    continue
+
+                if turn == self.__ENEMY_TURN:
+                    enemy_row, enemy_column, game_over = client.receive()
+                    self.__board.set_cell_state(enemy_row, enemy_column, Cell.O)
+
+                    if game_over == 1:
+                        you_lost_message = pygame.image.load("assets/you_lost.png")
+                        self.__window.blit(you_lost_message, (140, 40))
+                    turn = self.__MY_TURN
+                    continue
+
+                if event.type == MOUSEBUTTONDOWN:
+                    y, x = pygame.mouse.get_pos()
+
+                    if self.__click_on_board(x, y):
+                        row = self.__get_row(x)
+                        column = self.__get_column(y)
+
+                        if self.__board.get_cell_state(row, column) == Cell.Free:
+                            self.__board.set_cell_state(row, column, Cell.X)
+                            turn = self.__ENEMY_TURN
+                            if self.__board.free_cells():
+                                client.send(row, column, game_over)
+                            else:
+                                game_over = 1
+                                client.send(row, column, game_over)
+                                you_won_message = pygame.image.load("assets/you_won.png")
+                                self.__window.blit(you_won_message, (140, 40))
+                                self.__score += 1
---
+++ server.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+
+typedef int SOCKET;
+
+#define FIRST 1
+#define SECOND 0
+
+struct Move {
+    uint16_t row;
+    uint16_t column;
+    uint16_t winner;
+};
+
+struct Move receive_move_from_player(SOCKET player_socket) {
+    struct Move move;
+    
+    if(recv(player_socket, (char*) &move.row, sizeof(move.row), 0) < 0) {
+        printf("Error receiving the data\n");
+    }
+    if(recv(player_socket, (char*) &move.column, sizeof(move.column), 0) < 0) {
+        printf("Error receiving the data\n");
+    }
+    if(recv(player_socket, (char*) &move.winner, sizeof(move.winner), 0) < 0) {
+        printf("Error receiving the data\n");
+    }
+    
+    move.row = ntohs(move.row);
+    move.column = ntohs(move.column);
+    move.winner = ntohs(move.winner);
+    return move;
+}
+
+void send_move_to_player(SOCKET player_socket, struct Move move) {
+    move.row = htons(move.row);
+    move.column = htons(move.column);
+    move.winner = htons(move.winner);
+
+    if(send(player_socket, (char*) &move.row, sizeof(move.row), 0) < 0) {
+        printf("Error sending the data\n");
+    }
+    if(send(player_socket, (char*) &move.column, sizeof(move.column), 0) < 0) {
+        printf("Error sending the data\n");
+    }
+    if(send(player_socket, (char*) &move.winner, sizeof(move.winner), 0) < 0) {
+        printf("Error sending the data\n");
+    }
+}
+
+void handle_game(SOCKET first_player_socket, SOCKET second_player_socket) {
+    int16_t turn = FIRST;
+    turn = htons(turn);
+    send(first_player_socket, (char*) &turn, sizeof(turn), 0);
+    turn = SECOND;
+    turn = htons(turn);
+    send(second_player_socket, (char*) &turn, sizeof(turn), 0);
+
+
+    while(1) {
+        struct Move move = receive_move_from_player(first_player_socket);
+        send_move_to_player(second_player_socket, move);
+        struct Move move2 = receive_move_from_player(second_player_socket);
+        send_move_to_player(first_player_socket, move2);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    SOCKET server_socket;
+    struct sockaddr_in server, first_player, second_player;
+
+    if((server_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("Error creating the socket\n");
+        return 1;
+    }
+
+    memset(&server, 0, sizeof(server));
+    server.sin_port = htons(5240);
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+
+    if(bind(server_socket, (struct sockaddr* ) &server, sizeof(server)) < 0) {
+        printf("Error binding the socket\n");
+        return 1;
+    }
+
+    listen(server_socket, 1);
+
+    socklen_t size = sizeof(first_player);
+    SOCKET first_player_socket = accept(server_socket, (struct sockaddr *) &first_player, &size);
+    
+    if(first_player_socket < 0) {
+        printf("Error accepting\n");
+        return 1;
+    }
+    printf("First player connected with IP %s and PORT %d\n", inet_ntoa(first_player.sin_addr), ntohs(first_player.sin_port));
+
+    size = sizeof(second_player);
+    SOCKET second_player_socket = accept(server_socket, (struct sockaddr *) &second_player, &size);
+
+    if(second_player_socket < 0) {
+        printf("Error accepting\n");
+        return 1;
+    }
+    printf("Second player connected with IP %s and PORT %d\n", inet_ntoa(second_player.sin_addr), ntohs(second_player.sin_port));
+
+    handle_game(first_player_socket, second_player_socket);
+    close(first_player_socket);
+    close(second_player_socket);
+    close(server_socket);
+}
